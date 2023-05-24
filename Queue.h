c3eba5b58e844576492b0c1b7f74ac466968ceec
C4ptainHook <boykostudyinkpi@gmail.com>
2023-05-24 20:05:19 +0300

    It for Q

--- Queue.h
+++ Queue.h
@@ -1,5 +1,3 @@
-#ifndef SHUNTING_YARD_QUEUE_H
-#define SHUNTING_YARD_QUEUE_H
 #pragma once
 #include <iostream>
 #include <stdexcept>
@@ -17,11 +15,28 @@ class queue{
     Node* exit;
     unsigned int size;
 public:
+    class iterator{
+        Node* curr_ptr;
+    public:
+        iterator()=default;
+        iterator(*Node);
+        iterator& operator++();
+        const iterator operator++(int);
+        T operator*();
+        T* operator->();
+        bool operator==(const iterator&);
+        bool operator!=(const iterator&);
+
+        friend class queue<T>;
+    };
+public:
     queue();
     void push(T);
     void pop();
+    queue<T>::iterator begin();
+    queue<T>::iterator end();
 };
-
+//----------------------------------------------------------------Methods of queue
 template<class T>
 queue<T>::queue() {
     entrance = nullptr;
@@ -53,12 +68,49 @@ void queue<T>::pop() {
         delete temp;
         size--;
     }
-
 }
 
+template<class T>
+typename queue<T>::iterator queue<T>::begin() {
+    return iterator(entrance);
+}
 
+template<class T>
+typename queue<T>::iterator queue<T>::end() {
+    return iterator(exit);
+}
 
+//--------------------------------------------------------------------------Methods of iterator
 
+template<class T>
+typename queue<T>::iterator& queue<T>::iterator::operator++() {
+    this->curr_ptr=curr_ptr->next;
+    return *this;
+}
 
+template<class T>
+const typename queue<T>::iterator queue<T>::iterator::operator++(int) {
+    queue<T>::iterator temp(curr_ptr);
+    ++(*this);
+    return temp;
+}
 
-#endif //SHUNTING_YARD_QUEUE_H
+template<class T>
+T queue<T>::iterator::operator*() {
+    return curr_ptr->data;
+}
+
+template<class T>
+T* queue<T>::iterator::operator->() {
+    return &(curr_ptr->data);
+}
+
+template<class T>
+bool queue<T>::iterator::operator==(const queue<T>::iterator& other) {
+    return this->curr_ptr==other.curr_ptr;
+}
+
+template<class T>
+bool queue<T>::iterator::operator!=(const queue<T>::iterator& other) {
+    return this->curr_ptr!=other.curr_ptr;
+}
