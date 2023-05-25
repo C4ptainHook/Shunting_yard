068396854da44f416076df944a8805992655c0ba
C4ptainHook <boykostudyinkpi@gmail.com>
2023-05-26 02:35:30 +0300

    add sh yard

---
+++ Shunting_yard.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Parser.h"
+
+namespace Algorithm {
+    queue<Parser::Token> Shunting_yard(queue<Parser::Token>& parsed_q) {
+        queue<Parser::Token>::iterator it;
+        for (it = parsed_q.begin(); ) {
+
+
+        }
+    }
+
+}
