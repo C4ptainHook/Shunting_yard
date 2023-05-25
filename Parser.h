3a96fa71dedf18f1592ea91a5e51693744c062f7
C4ptainHook <boykostudyinkpi@gmail.com>
2023-05-26 00:29:58 +0300

    Logically finish parser and mod Q copy/move ctr + operators + clear method

--- Parser.h
+++ Parser.h
@@ -1,46 +1,39 @@
 #include <iostream>
 #include "Queue.h"
-#include "Stack.h"
+
+namespace Parser {
 
 
-class Token {
-public:
-    enum class Token_Type {
-        Undefined,
-        Number,
-        Operator,
-        LeftParen,
-        RightParen,
+    class Token {
+    public:
+        enum class Token_Type {
+            Undefined,
+            Number,
+            Operator,
+            LeftParen,
+            RightParen,
+        };
+
+        Token(Token_Type type,
+              const std::string &_str,
+              int precedence = -1,
+              bool rightAssociative = false,
+              bool unary = false
+        )
+                : type{type}, str{_str}, precedence{precedence}, is_rightAssociative{rightAssociative}, unary{unary} {}
+
+        const Token_Type type;
+        const std::string str;
+        const int precedence;
+        const bool is_rightAssociative;
+        const bool unary;
     };
 
-    Token(Token_Type type,
-          const std::string& _str,
-          int precedence = -1,
-          bool rightAssociative = false,
-          bool unary = false
-    )
-            : type { type }
-            , str {_str }
-            , precedence { precedence }
-            , is_rightAssociative { rightAssociative }
-            , unary { unary }
-    {}
 
-    const Token_Type type;
-    const std::string str;
-    const int precedence;
-    const bool is_rightAssociative;
-    const bool unary;
-};
+    std::ostream &operator<<(std::ostream &os, const Token &token) {
+        os << token.str;
+        return os;
+    }
 
-std::ostream& operator<<(std::ostream& os, const Token& token) {
-    os << token.str;
-    return os;
+    queue<Token> Parse(const char*);
 }
-
-class Parser {
-    std::string expression;
-public:
-    explicit Parser(char * _expr): expression(_expr) {}
-    queue<Token<std::string>> parse_data();
-};
